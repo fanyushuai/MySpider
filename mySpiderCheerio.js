4fba69928bf830759ba946783f2e035157e2bebd
fanyushuai <272253157@qq.com>
2017-12-19 20:47:22 +0800

    123

--- mySpiderCheerio.js
+++ mySpiderCheerio.js
@@ -23,8 +23,8 @@ function startRequest(x) {
 		});
 		// 监听end事件，如果整个网页内容的html都获取完毕，就执行回调函数
 		res.on('end', function() {
-
 			var $ = cheerio.load(html); // 采用cheerio模块解析html
+			console.log($("GV.DETAIL").val());
 			var time = $('.meta span').eq(0).text().trim();
 			var title = $(".title h1").text().trim();
 			var author_name = $(".source").text().trim();
@@ -47,5 +47,15 @@ function startRequest(x) {
 
 }
 
+function testRequest(){
+	request("http://hot.cnbeta.com/comment/read?_csrf=dLvbbJbTgE7GMekonXjgeWrmy25-oyl-5rrdGnmiPCLYqH8ttIcAtT88c9eZNi-OiW6pyO3SVYldzk7SmrctjA%3D%3D&op=1%2C680207%2C7b6a3", function (error, response, body) {
+	    if (!error && response.statusCode === 200) {
+	    	data = JSON.parse(body);
+	    	console.log(data);
+	    } else {
+	    }
+	});
+}
 
-fetchPage(url); // 主程序开始运行
+testRequest();
+//fetchPage(url); // 主程序开始运行
